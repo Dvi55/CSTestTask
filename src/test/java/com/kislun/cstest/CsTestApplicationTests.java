b26f4a47593d402bf08d916b60819cf93640bb4d
Andrii <kisluna789@gmail.com>
2024-05-02 16:28:26 +0300

    Init project

---
+++ src/test/java/com/kislun/cstest/CsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kislun.cstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
